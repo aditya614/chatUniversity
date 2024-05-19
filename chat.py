0d61d771de55e6638d8faeff26f0fd9bd4c4ba96
Aditya kumar <75215840+aditya614@users.noreply.github.com>
2024-05-19 15:06:08 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,49 @@
+import random #for random choice of answer
+import json
+
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)#info to create model
+model.load_state_dict(model_state)#to load dictionary
+model.eval()# to evolve model
+
+bot_name = "Aditya"
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])# reshaping in 1 row and multiple column
+    X = torch.from_numpy(X).to(device)#because bag of words func return a numpy array
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses']) #random choice from response array
+        
+                return  "I do not understand..."
+    
